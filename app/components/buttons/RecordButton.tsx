e0866fa541ac1f0aeb7d9b791dc58e82774d7b49
Hector Ramirez <hector.ramirez.her20@gmail.com>
2026-04-18 04:05:06 -0600

    page consulta

---
+++ app/components/buttons/RecordButton.tsx
@@ -0,0 +1,33 @@
+// components/RecordButton.tsx
+'use client';
+
+import { useAudioRecorder } from '../../../hook/useAudioRecorder';
+import { useRecordingStore } from '../../../store/useRecordingStore';
+
+export default function RecordButton() {
+    const { isRecording } = useRecordingStore();
+    const { start, stop } = useAudioRecorder();
+
+    return (
+        <div className="flex flex-col items-center gap-3">
+            <button
+                onClick={isRecording ? stop : start}
+                className={`relative flex items-center justify-center w-20 h-20 rounded-full text-white transition-all duration-300 shadow-lg ${isRecording
+                    ? 'bg-red-500 hover:bg-red-600 animate-pulse'
+                    : 'bg-blue-600 hover:bg-blue-700'
+                    }`}
+            >
+                {/* Ícono simple (puedes cambiarlo por lucide-react o heroicons luego) */}
+                {isRecording ? (
+                    <div className="w-6 h-6 bg-white rounded-sm"></div> // Cuadrado de Stop
+                ) : (
+                    <div className="w-6 h-6 bg-white rounded-full"></div> // Círculo de Rec
+                )}
+            </button>
+
+            <span className={`text-sm font-medium ${isRecording ? 'text-red-500' : 'text-gray-500'}`}>
+                {isRecording ? 'Grabando consulta...' : 'Iniciar consulta'}
+            </span>
+        </div>
+    );
+}
